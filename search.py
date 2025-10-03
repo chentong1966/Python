2511530a6b3b4e07279da511c106af229de423b1
chentong1966 <chentong@usa.com>
2025-10-03 19:33:47 -0400

    Update search.py

--- search.py
+++ search.py
@@ -16,7 +16,27 @@ def binarySearch(a,x) -> int|None :
         if(j==None):
             return None
         else:
-            return k+1 + binarySearch(a[k+1:],x) 
+            return k+1 + binarySearch(a[k+1:],x)            
 for x in a:     
  print("search: ", x," return: ",binarySearch(a, x), a)
 print("search: ", 30," return: ",binarySearch(a, 30), a)
+
+#BinarySearch
+def binarysearch(a, low, high, target):
+    print(a, low, high, target)
+    if low > high:
+        return -1
+    k=(low+high)//2
+    if(a[k] == target):
+        print(f"find {a[k]} at [{k}]")
+        return k
+    elif a[k] < target:
+        return binarysearch(a, k + 1 , high, target)
+    elif a[k] > target:
+        return binarysearch(a, low , k-1, target)
+        
+
+a=[2, 3, 3, 3, 4, 5, 5, 5, 7, 323, 56435]
+for x in a:     
+ print("search: ", x," return: ",binarysearch(a, 0,len(a)-1,x), a)
+print("search: ", 30," return: ",binarysearch(a, 0,len(a)-1,30), a)
