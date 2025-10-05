b7f0ca36639bd472a6e6f50cb80000626c9d0fb2
chentong1966 <chentong@usa.com>
2025-10-04 20:46:23 -0400

    Update search.py

--- search.py
+++ search.py
@@ -40,3 +40,49 @@ a=[2, 3, 3, 3, 4, 5, 5, 5, 7, 323, 56435]
 for x in a:     
  print("search: ", x," return: ",binarysearch(a, 0,len(a)-1,x), a)
 print("search: ", 30," return: ",binarysearch(a, 0,len(a)-1,30), a)
+
+#DFS
+def dfs(a,v,x):
+    print("visited", visited)
+    print("visiting",v)
+    if a[v] == x:
+        return v , a[v]
+    visited.add(v)
+    for e in edgesOf(v):
+        print("explored",e, "explored", explored)
+        if(e not in explored):
+            explored.add(e)
+            u,w = e.split("-")
+            if(int(w) not in visited):
+                print("discover edge:",e)
+                r = dfs(a,int(w),x)
+                if(r != None):
+                    return r
+            else:
+                print("back track edge:",e)
+
+def edgesOf(v) -> set:
+    myedges:set=set()
+    if(v!=0):
+        if(v%2==0):
+            myedges.add(str(v//2-1)+"-"+str(v))
+        else:
+           myedges.add(str(v//2)+"-"+str(v)) 
+    if 2*v+1 < len(a):
+        myedges.add(str(v)+"-"+str(2*v+1))
+    if 2*v+2 < len(a):
+        myedges.add(str(v)+"-"+str(2*v+2))
+    print("edges of", v, myedges)
+    return myedges
+    
+    
+    
+a = [2, 3, 3, 3, 4, 5, 5, 5, 7, 323, 56435,8]
+for x in reversed(a):
+    visited:set = set()
+    explored:set = set()
+    print("search: ", x," return: ",dfs(a,0,x), a,"\n")
+print("search: ", 30," return: ",dfs(a,0,30), a)
+    explored:set = set()
+    print("search: ", x," return: ",dfs(a,0,x), a)
+print("search: ", 30," return: ",dfs(a,0,30), a)
