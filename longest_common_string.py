bb570c2e86f8b527ac37d45c3e0e268816ea63c8
chentong1966 <chentong@usa.com>
2025-10-06 19:24:48 -0400

    Update longest_common_string.py

--- longest_common_string.py
+++ longest_common_string.py
@@ -14,4 +14,18 @@ for i in range(0,len(x)):
             cls[(i,j)] = cls[(i-1,j-1)]+1
         else:
             cls[(i,j)] = max(cls[(i-1,j)],cls[(i,j-1)])
+print("longest=",cls[(len(x)-1,len(y)-1)])
 print(cls)
+
+def backtrack(i,j):
+    if i==-1 or j==-1:
+        return ""
+    if x[i] == y[j]:
+        return backtrack(i-1,j-1) + x[i]
+    if cls[(i,j-1)] > cls[(i-1,j)]:
+        return backtrack(i,j-1)
+    return backtrack(i-1,j)
+    
+clss = backtrack(len(x)-1,len(y)-1)    
+
+print(clss)
